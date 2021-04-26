aa15781bf16e22578c5d39f8850baf1981129a51
ggicalaa <81258328+ggicalaa@users.noreply.github.com>
2021-04-27 01:19:48 +0200

    Update description.R

--- description.R
+++ description.R
@@ -1,1 +1,21 @@
-#' 
+#Package: mil_do_km
+
+#Title: Zamiana Mil Lądowych Na Kilometry
+
+#Version: 0.0.1
+
+#Authors@R:
+#   person (given = "Dominika",
+#           family = "Gicala",
+#           role = c ("cre", "aut"),
+#           email = "domgic@st.amu.edu.pl")
+
+#Description: Przeliczanie wartości podanych w milach lądowych na kilometry
+
+#License: CCO
+
+#Encoding: UTF-8
+
+#LazyData: true
+
+#RxygenNote: 6.1.1
